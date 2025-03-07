a02cd13a193d529896225c3fa2cdbf750425a09a
Evave1204 <chengjiehong74@gmail.com>
2025-03-08 04:51:18 +0800

    foundation fno prepared

--- compression/utils.py
+++ compression/utils.py
@@ -63,9 +63,13 @@ def evaluate_model(model, dataloader, data_processor, device='cuda', track_perfo
 
     with torch.no_grad():
         for batch in dataloader:
+            # batch : {"x":, "y":}
+            # print(batch.keys())
+            # return
             if data_processor is not None:
                 processed_data = data_processor.preprocess(batch)
             else:
+
                 processed_data = {k: v.to(device)
                                   for k, v in batch.items() if torch.is_tensor(v)}
             
@@ -95,7 +99,7 @@ def evaluate_model(model, dataloader, data_processor, device='cuda', track_perfo
             # Track runtime
             if track_performance:
                 start_time = time.time()
-            
+
             out = model(**processed_data)
             
             if track_performance:
@@ -297,3 +301,52 @@ def optional_fno(resolution):
             encode_output=True,
         )
         return fno_model, train_loader, test_loaders, data_processor
+    
+from ruamel.yaml import YAML
+import logging
+
+class YParams():
+  """ Yaml file parser """
+  def __init__(self, yaml_filename, config_name, print_params=False):
+    self._yaml_filename = yaml_filename
+    self._config_name = config_name
+    self.params = {}
+
+    if print_params:
+      print("------------------ Configuration ------------------")
+
+    with open(yaml_filename, encoding='utf-8') as _file:
+
+      for key, val in YAML().load(_file)[config_name].items():
+        if print_params: print(key, val)
+        if val =='None': val = None
+
+        self.params[key] = val
+        self.__setattr__(key, val)
+
+    if print_params:
+      print("---------------------------------------------------")
+
+  def __getitem__(self, key):
+    return self.params[key]
+
+  def __setitem__(self, key, val):
+    self.params[key] = val
+    self.__setattr__(key, val)
+
+  def __contains__(self, key):
+    return (key in self.params)
+
+  def update_params(self, config):
+    for key, val in config.items():
+      self.params[key] = val
+      self.__setattr__(key, val)
+
+  def log(self):
+    logging.info("------------------ Configuration ------------------")
+    logging.info("Configuration file: "+str(self._yaml_filename))
+    logging.info("Configuration name: "+str(self._config_name))
+    for key, val in self.params.items():
+        logging.info(str(key) + ' ' + str(val))
+    logging.info("---------------------------------------------------")
+
